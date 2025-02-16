78a9bbaa51cf80189d32305be2b3bbb6147703b3
Muhammad Muneeb Ali <159451843+MUNEEBALI-DEV@users.noreply.github.com>
2025-02-16 17:52:31 +0500

    First Upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+    $('#home').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        smartSpeed:1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        navText:['Prev','Next'],
+        responsive:{
+            0:{
+                items:1
+            },
+           
+        }
+    })
+    $('#portfolio-cards').owlCarousel({
+        loop:true,
+        margin:10,
+        dots:true,
+        smartSpeed:1000,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        responsive:{
+            0:{
+                items:1,
+                margin:0,
+            },
+            768:{
+                items:1,
+                center:false,
+                margin:0,
+            },
+            1100:{
+               items:2,
+                center:true,
+            },
+           
+        }
+    })
+    $('#reviews-cards').owlCarousel({
+        loop:true,
+        margin:0,
+        dots:true,
+        smartSpeed:1000,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:false,
+        responsive:{
+           0:{
+            items:1,
+           }
+        }
+    })
+
+
+  
